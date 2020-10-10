b7529fd8e655b95e30d3ed945aa177ea22a2ec35
Andrey Maslov <andrey.maslov@actum.cz>
2020-10-10 21:23:24 +0200

    first deployable version

--- src/main/java/com/liferay/docs/mvcportlet/portlet/NewsPortlet.java
+++ src/main/java/com/liferay/docs/mvcportlet/portlet/NewsPortlet.java
@@ -1,30 +1,45 @@
 package com.liferay.docs.mvcportlet.portlet;
 
 import com.liferay.docs.mvcportlet.constants.NewsPortletKeys;
-
+import com.liferay.portal.kernel.model.User;
 import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.WebKeys;
+import org.osgi.service.component.annotations.Component;
 
 import javax.portlet.Portlet;
-
-import org.osgi.service.component.annotations.Component;
+import javax.portlet.PortletException;
+import javax.portlet.RenderRequest;
+import javax.portlet.RenderResponse;
+import java.io.IOException;
 
 /**
  * @author Andrey
  */
 @Component(
-	immediate = true,
-	property = {
-		"com.liferay.portlet.display-category=category.sample",
-		"com.liferay.portlet.header-portlet-css=/css/main.css",
-		"com.liferay.portlet.instanceable=true",
-		"javax.portlet.display-name=NewsPortlet",
-		"javax.portlet.init-param.template-path=/",
-		"javax.portlet.init-param.view-template=/view.jsp",
-		"javax.portlet.name=" + NewsPortletKeys.NEWSPORTLET,
-		"javax.portlet.resource-bundle=content.Language",
-		"javax.portlet.security-role-ref=power-user,user"
-	},
-	service = Portlet.class
+        immediate = true,
+        property = {
+                "com.liferay.portlet.display-category=actum",
+                "com.liferay.portlet.header-portlet-css=/css/main.css",
+                "com.liferay.portlet.instanceable=true",
+                "javax.portlet.display-name=NewsPortlet",
+                "javax.portlet.init-param.template-path=/",
+                "javax.portlet.init-param.view-template=/view.jsp",
+                "javax.portlet.name=" + NewsPortletKeys.NEWSPORTLET,
+                "javax.portlet.resource-bundle=content.Language",
+                "javax.portlet.security-role-ref=power-user,user"
+        },
+        service = Portlet.class
 )
 public class NewsPortlet extends MVCPortlet {
+
+
+    @Override
+    public void render(RenderRequest renderRequest, RenderResponse renderResponse) throws IOException, PortletException {
+        ThemeDisplay themeDisplay = (ThemeDisplay) renderRequest.getAttribute(WebKeys.THEME_DISPLAY);
+        User user = themeDisplay.getUser();
+        renderRequest.setAttribute("userName", user.getFirstName());
+        super.render(renderRequest, renderResponse);
+    }
+
 }
