aeef38e4eb6022e5fa5553e9511b30ac6ba01cb6
Andrey Maslov <andrey.maslov@actum.cz>
2020-10-12 15:21:34 +0200

    pre-final

--- src/main/java/cz/actum/portlet/news/NewsPortlet.java
+++ src/main/java/cz/actum/portlet/news/NewsPortlet.java
@@ -3,7 +3,10 @@ package cz.actum.portlet.news;
 import com.gargoylesoftware.htmlunit.BrowserVersion;
 import com.gargoylesoftware.htmlunit.WebClient;
 import com.gargoylesoftware.htmlunit.html.HtmlDivision;
+import com.gargoylesoftware.htmlunit.html.HtmlHeading2;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
+import com.gargoylesoftware.htmlunit.html.HtmlParagraph;
+import com.gargoylesoftware.htmlunit.html.HtmlSpan;
 import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
 import com.liferay.portal.kernel.util.ParamUtil;
 import cz.actum.portlet.constants.NewsPortletKeys;
@@ -16,6 +19,7 @@ import javax.portlet.PortletException;
 import javax.portlet.RenderRequest;
 import javax.portlet.RenderResponse;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -42,23 +46,56 @@ public class NewsPortlet extends MVCPortlet {
     private String from = "not set";
     private String to = "not set";
 
+    static List<Connection> getConnectionsFromIdos(String idosUrl) throws IOException {
+        WebClient webClient = new WebClient(BrowserVersion.CHROME, false, null, 0);
+        HtmlPage page = webClient.getPage(idosUrl);
+        List<HtmlDivision> elements = page.getByXPath("//div[contains(@class, 'box connection detail-box ca-collapsed')]");
+        return processConnectionBoxes(elements);
+    }
+
+    static List<Connection> processConnectionBoxes(List<HtmlDivision> boxes) {
+        List<Connection> connections = new ArrayList<Connection>();
+        for (HtmlDivision box : boxes) {
+            connections.add(processConnectionBox(box));
+        }
+        return connections;
+    }
+
+    static Connection processConnectionBox(HtmlDivision box) {
+        HtmlHeading2 from = (HtmlHeading2) box.getByXPath("(//h2[contains(@class, 'reset date')])").get(0);
+        String fromText = from.getFirstChild().asText();
+        HtmlSpan date = (HtmlSpan) box.getByXPath("(//span[contains(@class, 'date-after')])").get(0);
+        String dateText = date.getFirstChild().asText();
+        HtmlParagraph to = (HtmlParagraph) box.getByXPath("(//p[contains(@class, 'reset time')])").get(1);
+        String toText = to.getFirstChild().asText();
+        return new Connection("", dateText, fromText, toText);
+    }
+
     @Override
     public void render(RenderRequest renderRequest, RenderResponse renderResponse) throws IOException, PortletException {
         renderRequest.setAttribute("from", from);
         renderRequest.setAttribute("to", to);
+        renderRequest.setAttribute("connections", getConnectionsFromIdos("https://idos.idnes.cz/mladaboleslav/spojeni/vysledky/?f=" + from + "&fc=333003&t=" + to + "&tc=333003"));
         super.render(renderRequest, renderResponse);
     }
 
     public void setStops(ActionRequest actionRequest, ActionResponse actionResponse) throws IOException, PortletException {
         from = ParamUtil.getString(actionRequest, "from");
         to = ParamUtil.getString(actionRequest, "to");
-        parsePage(from, to);
     }
 
-    private void parsePage(String from, String to) throws IOException {
-        WebClient webClient = new WebClient(BrowserVersion.CHROME, false, null, 0);
-        HtmlPage page = webClient.getPage("https://idos.idnes.cz/mladaboleslav/spojeni/vysledky/?f=" + from + "&fc=333003&t=" + to + "&tc=333003");
-        List<HtmlDivision> elements = page.getByXPath("//div[contains(@class, 'box connection detail-box ca-collapsed')]");
-    }
 
+    public static class Connection {
+        private String type;
+        private String date;
+        private String from;
+        private String to;
+
+        public Connection(String type, String date, String from, String to) {
+            this.type = type;
+            this.date = date;
+            this.from = from;
+            this.to = to;
+        }
+    }
 }
--- src/main/resources/META-INF/resources/view.jsp
+++ src/main/resources/META-INF/resources/view.jsp
@@ -4,9 +4,83 @@
 <jsp:useBean id="from" type="java.lang.String" scope="request" />
 <jsp:useBean id="to" type="java.lang.String" scope="request" />
 
-<b>From: <%=from %></b>
-<i>To: <%=to %></i>
+
+<style type="text/css">
+	.wrapper {
+		width: 45%;
+		font-family: sans-serif;
+	}
+
+	.departures thead td {
+		padding: 1em;
+		background-color: #e3eff8;
+		font-weight: bold;
+	}
+	.departures tbody td {
+		border-bottom: 1px solid #98c6e3;
+		padding: 0.8em 1em;
+	}
+
+	.departures {
+		width: 100%;
+		border: 5px solid #98c6e3;
+	}
+
+	.from, .to {
+		font-size: 1.2em;
+		font-weight: bold;
+		margin: 0 0.25em 0 0.35em;
+	}
+
+	.to {
+		margin-right: 0.8em;
+	}
+</style>
+
+
+<div class="wrapper">
+	<p>Detail vašeho spojení ze stanice <span class="from"><%= from %></span> do stanice <span class="to"><%= to %></span>:</p>
+
+	<table class="departures" cellspacing="0">
+		<thead>
+		<tr>
+			<td>Odjezd</td>
+			<td>Typ spoje</td>
+			<td>Doba jízdy</td>
+		</tr>
+		</thead>
+		<tbody>
+		<tr>
+			<td>
+				<div>2. 10. 2020</div>
+				<div>13:44</div>
+			</td>
+			<td>
+				<div>vlak, autobus</div>
+			</td>
+			<td>
+				<div>20 minut</div>
+			</td>
+		</tr>
+		<tr>
+			<td>
+				<div>2. 10. 2020</div>
+				<div>13:44</div>
+			</td>
+			<td>
+				<div>vlak, autobus</div>
+			</td>
+			<td>
+				<div>20 minut</div>
+			</td>
+		</tr>
+		</tbody>
+	</table>
+</div>
+
+
 <portlet:actionURL name="setStops" var="setStopsURL"></portlet:actionURL>
+
 <aui:form action="<%= setStopsURL %>" name="<portlet:namespace />fm">
 	<aui:fieldset>
 		<aui:input name="from"></aui:input>
